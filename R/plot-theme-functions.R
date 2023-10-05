2ba376b33ed965191d58fcfecfdb596a5520a92d
Luke Shaw <luke.francis.shaw@gmail.com>
2023-10-05 12:08:08 +0100

    plotting and examples

---
+++ R/plot-theme-functions.R
@@ -0,0 +1,71 @@
+# taken from BNSSG_theme_package by DanMaloneyBNSSG 2023-09-27
+bnssgtheme <- function(base_size = 12, base_family = "sans",base_colour = "black"){theme_bw() %+replace% theme(
+  axis.title.x = element_text(size = 16, color = '#003087', face = 'bold', family = "sans", margin = margin(t = 0, r = 20, b = 0, l = 0)), #x Axis Titles
+  axis.title.y = element_text(size = 16, color = '#003087', angle = 90, face = 'bold', family = "sans", margin = margin(t = 0, r = 20, b = 0, l = 0)), #y Axis Titles
+  axis.text = element_text(size = 12,  family = "sans", color = 'black'), #Axis text
+  panel.border = element_blank(), #remove plot border
+  panel.grid.major.x = element_blank(), #no major vertical lines
+  panel.grid.major.y = element_line(linetype = 'dotted', size = 1), #dotted major horizontal lines
+  panel.grid.minor = element_blank(), #no minor lines
+  legend.position = "top", #legend on top
+  legend.justification='left', #legend left
+  legend.direction='horizontal', #legend to be horizontal
+  legend.title = element_blank(), #No legend title
+  legend.text = element_text(size = 12, family = "sans",),
+  legend.key.size = unit(0.3, "cm"),
+  plot.title = element_text(size = 16, color = '#003087', face="bold", margin = margin(b = 10, t=10), hjust=0),
+  plot.subtitle = element_text(size = 10, margin = margin(b = 10), hjust=0)
+)
+}
+
+
+bnssgcols <- c(`white` = "#FFFFFF",`light grey` = "#999999",`light pink` = "#D093B6",`mid pink` = "#8d488d",`light blue` = "#8AC0E5",`dark pink` = "#853358",
+               `mid blue` = "#2472AA",`dark blue` = "#003087",`dark grey` = "#333333")
+
+##Tells R Where to look for color codes, from the name
+bnssg_cols <- function(...) {
+  cols <- c(...)
+
+  if (is.null(cols))
+    return (bnssgcols)
+
+  bnssgcols[cols]
+}
+
+##Set colour 'names'scheme' names:
+bnssg_palettes <- list(
+  "main"  = bnssg_cols("light grey", "light pink", "mid pink", "light blue", "mid blue", "dark blue", "dark grey","dark pink"),
+  "pgb" = bnssg_cols("dark pink", "mid pink", "light pink", "light grey", "light blue", "mid blue", "dark blue"),
+  "pgblite" = bnssg_cols("dark pink", "light grey", "dark blue"),
+  "pink" = bnssg_cols("dark pink", "mid pink", "light pink"),
+  "blue" = bnssg_cols("dark blue", "mid blue", "light blue")
+)
+
+##Function so that the colours can be found from the scheme name
+bnssg_pal <- function(palette, reverse = FALSE, ...) {
+  pal <- bnssg_palettes[[palette]]
+
+  if (reverse) pal <- rev(pal)
+
+  colorRampPalette(pal, ...)
+}
+
+scale_colour_bnssg <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- bnssg_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    discrete_scale("color", paste0("bnssg_", palette), palette = pal, ...)
+  } else {
+    scale_colour_gradientn(colors = pal(256), ...)
+  }
+}
+
+scale_fill_bnssg <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- bnssg_pal(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    discrete_scale("fill", paste0("bnssg_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
---
+++ inst/Workflow_Example/three_example_transitions.R
@@ -0,0 +1,104 @@
+library(dpm)
+library(magrittr)
+# parameter define --------------------------------------------------------
+
+
+initial_population <- tibble::tribble(
+  ~state_name, ~initial_pop,
+  "CS1",2000,
+  "CS2",1000,
+  "CS3",500,
+  "CS4",200,
+  "CS5",100
+)
+# define the initial matrix
+inner_trans_matrix_default <- matrix(
+  data = c(0.8,0.2,0,0,0,
+           0.2,0.8,0,0,0,
+           0.0,0,1,0,0,
+           0.0,0,0,1,0,
+           0.0,0,0,0,1),
+  nrow = 5,
+  ncol = 5)
+
+total_time <- 20
+
+births_migrations_deaths_figures <- tibble::tibble(
+  year = rep(1:total_time,each=3),
+  event = rep(c("births","migrations","deaths"),times=total_time),
+  value = rep(c(100,10,50),times=total_time)
+)
+
+birth_migration_deaths_proportions <- tibble::tribble(
+  ~state_name, ~event, ~prop,
+  "CS1","births",0.9,
+  "CS2","births",0.07,
+  "CS3","births",0.03,
+  "CS4","births",0,
+  "CS5","births",0,
+  "CS1","migrations",0.5,
+  "CS2","migrations",0.3,
+  "CS3","migrations",0.2,
+  "CS4","migrations",0.09,
+  "CS5","migrations",0.01,
+  "CS1","deaths",0.05,
+  "CS2","deaths",0.05,
+  "CS3","deaths",0.15,
+  "CS4","deaths",0.25,
+  "CS5","deaths",0.5)
+
+
+# run scenarios -----------------------------------------------------------
+
+
+# BASE SCENARIO
+dpm::run_dpm(
+  initial_population = initial_population,
+  inner_trans_matrix_list = inner_trans_matrix_default,
+  total_time = total_time,
+  births_migrations_deaths_figures = births_migrations_deaths_figures,
+  birth_migration_deaths_proportions = birth_migration_deaths_proportions) %>%
+  # plot the Sankey
+  create_sankey(inner_trans_matrix_list=inner_trans_matrix_default) +
+  labs(title = "BASE SCENARIO - no change to inner trans \n \n")
+
+# ONE CHANGE
+inner_trans_matrix_1change <- inner_trans_matrix_default %>%
+  # make a change
+  changing_inner_trans_matrix(from_cs = 1,
+                              to_cs = 2,
+                              scalar_change = 0.9,
+                              over_n_iterations = 5)
+dpm::run_dpm(
+  initial_population = initial_population,
+  inner_trans_matrix_list = inner_trans_matrix_1change,
+  total_time = total_time,
+  births_migrations_deaths_figures = births_migrations_deaths_figures,
+  birth_migration_deaths_proportions = birth_migration_deaths_proportions) %>%
+  # plot the Sankey
+  create_sankey(inner_trans_matrix_list=inner_trans_matrix_1change) +
+  labs(title = "BASE + \nCS1-->CS2 10% transition reduction in 5 years \n")
+
+# TWO CHANGES
+inner_trans_matrix_2changes <- inner_trans_matrix_1change %>%
+  # make another change
+  changing_inner_trans_matrix(
+    from_cs = 2,
+    to_cs = 1,
+    scalar_change = 0,
+    over_n_iterations = 10)
+dpm::run_dpm(
+  initial_population = initial_population,
+  inner_trans_matrix_list = inner_trans_matrix_2changes,
+  total_time = total_time,
+  births_migrations_deaths_figures = births_migrations_deaths_figures,
+  birth_migration_deaths_proportions = birth_migration_deaths_proportions) %>%
+  # plot the Sankey
+  create_sankey(inner_trans_matrix_list=inner_trans_matrix_2changes) +
+  labs(title = paste0("BASE + ",
+                      "\nCS1-->CS2 10% transition reduction in 5 years + ",
+                      "\nCS2-->CS1 100% transition reduction in 10 years"))
+
+
+
+
