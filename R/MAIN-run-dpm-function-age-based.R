307cd2b886fd394ecb3425b484327dafef44186c
Luke Shaw <luke.francis.shaw@gmail.com>
2024-08-13 09:52:07 +0100

    fixing NA in run_dpm_age_based
    And making print options nicer

--- R/MAIN-run-dpm-function-age-based.R
+++ R/MAIN-run-dpm-function-age-based.R
@@ -27,17 +27,33 @@ get_age_based_dpm_inputs_from_folder <- function(folder){
   return(inputs)
 }
 
+log_and_print_intermediate_results <- function(log_list,
+                                               message,
+                                               print_intermediate_results){
+  # add the message to the loglist
+  log_list <- append(log_list, message)
+  # print into console if desired
+  if(print_intermediate_results){print(message)}
+  return(log_list)
+}
+
 
 #' run_dpm main function for running the dpm model
 #' @param folder folder to find all information from
-#' @param return_births_net_migration_deaths_in_output boolean if TRUE then list returned with named outputs
+#' @param return_births_net_migration_deaths_in_output boolean if TRUE then list
+#' @param print_intermediate_results boolean if TRUE then outputs as it goes
+#' returned with named outputs
 #' @export
 #' @import dplyr
-run_dpm_age_based <- function(folder){
+run_dpm_age_based <- function(folder, print_intermediate_results=T){
+  # start by initialising the log list
+  log_list <- list()
+
   # read in the inputs
   inputs <- get_age_based_dpm_inputs_from_folder(folder)
 
-  deaths_method = "Keep CS expected deaths same as current year and don't align to ONS"
+  deaths_method = paste0(
+    "Keep CS expected deaths same as current year and don't align to ONS")
 
   year_range <- (inputs$config$baseline_year+1):inputs$config$final_year
 
@@ -46,11 +62,14 @@ run_dpm_age_based <- function(folder){
     count(state_name, age, age_group) |>
     rename(initial_pop = n) |>
     complete(state_name, age, age_group, fill = list(initial_pop = 0)) |>
-    # remove completed rows where first character of age is not first character of age_group
+    # remove completed rows where first character of age is not first character
+    # of age_group
     filter(stringr::str_sub(age,1,1) == stringr::str_sub(age_group,1,1) |
              # annoying "17-29" category
-             (stringr::str_sub(age,1,1)=="1" & stringr::str_sub(age_group,1,1)=="2") |
-             (stringr::str_sub(age,1,1)=="2" & stringr::str_sub(age_group,1,1)=="1")) |>
+             (stringr::str_sub(age,1,1)=="1" &
+                stringr::str_sub(age_group,1,1)=="2") |
+             (stringr::str_sub(age,1,1)=="2" &
+                stringr::str_sub(age_group,1,1)=="1")) |>
     # stupid situation of those in their hundreds given 17-29 cat
     filter(!(nchar(age)==3 & age_group=="17-29")) |>
     arrange(age, state_name)
@@ -58,19 +77,23 @@ run_dpm_age_based <- function(folder){
   # work out the proportion of event for each state name and age
   birth_im_em_death_probs <- inputs$birth_im_em_death_probs |>
     group_by(event) |>
-    mutate(prop_of_total_event = num_people_had_event / sum(num_people_had_event)) |>
+    mutate(prop_of_total_event =
+             num_people_had_event / sum(num_people_had_event)) |>
     ungroup()
-  # misnoma in that we are NOT combining to net migration - we're leaving as immigration and emigration
+  # misnoma in that we are NOT combining to net migration - we're leaving as
+  # immigration and emigration
   births_net_migration_deaths_by_CS <- inputs$ons_birth_im_em_death_nums |>
     select(year, event, ons_num_people=value) |>
-    left_join(birth_im_em_death_probs |> select(event, state_name, age, age_group, prop_of_total_event),
+    left_join(birth_im_em_death_probs |> select(event, state_name, age,
+                                                age_group, prop_of_total_event),
               by = c("event"),
               relationship="many-to-many") |>
     mutate(value = prop_of_total_event * ons_num_people) |>
     select(year, event, state_name, age, age_group, value)
 
 
-  # create the population table - only first year filled in, by initial_population
+  # create the population table - only first year filled in, by
+  # initial_population
   population_at_each_year <-initial_population |>
     mutate(year=inputs$config$baseline_year) |>
     rename(population=initial_pop)
@@ -84,7 +107,11 @@ run_dpm_age_based <- function(folder){
 
   zero_year_check <- 0
   for (i in year_range) {
-    print(paste0("starting year",i))
+    # log the start of the year
+    log_list <- log_and_print_intermediate_results(
+      log_list,
+      paste0("*** STARTING YEAR ",i),
+      print_intermediate_results)
 
     inner_trans_long_tbl_i <- inner_trans_long_tbl |> filter(year==i)
 
@@ -105,15 +132,18 @@ run_dpm_age_based <- function(folder){
         by=c("state_name","age","age_group")) |>
       mutate(population = population-value,
              year = i) |>
+      mutate(population = replace_na(population,0)) |>
       select(year, state_name, age, age_group, population)
 
-    # check we haven't oversteppd
+    # check we haven't overstepped
     if(prev_pop_minus_emigration |> filter(population<0) |>  nrow()){
       num_neg <- prev_pop_minus_emigration |>
         filter(population<0) |>
         summarise(a=sum(population)) |>  pull(a)
-      warning(paste0("emigrations led to negative numbers (",abs(round(num_neg)), " in total) - have reset to 0"))
-      prev_pop_minus_emigration <- prev_pop_minus_emigration
+      warning(paste0("emigrations led to negative numbers (",abs(round(num_neg)),
+                     " in total) - have reset to 0"))
+      prev_pop_minus_emigration <- prev_pop_minus_emigration |>
+        mutate(population = ifelse(population<0,0,population))
     }
     # count the differences
     pop_prev <- prev_pop |>  summarise(a=sum(population)) |>  pull(a)
@@ -123,8 +153,15 @@ run_dpm_age_based <- function(folder){
     emigrations_expected <- births_net_migration_deaths_by_CS |>
       filter(event=="emigrations",year==i) |> summarise(a=sum(value)) |> pull(a)
 
-    print(paste0(round(emigrations_expected)," expected"))
-    print(paste0(round(emigrations_actual)," actual"))
+    # log list
+    log_list <- log_and_print_intermediate_results(
+      log_list,
+      paste0("Emigrations: ",
+             round(emigrations_expected)," expected, ",
+             round(emigrations_actual)," actual"),
+      print_intermediate_results)
+
+
 
     #####
     # sample health state change (including deaths)
@@ -145,12 +182,19 @@ run_dpm_age_based <- function(folder){
       dpm:::convert_to_decade("age_group")
 
     # remove the deaths
-    died <- prev_pop_minus_emigration_with_transitions |> filter(state_name=="Died")
+    died <- prev_pop_minus_emigration_with_transitions |>
+      filter(state_name=="Died")
     prev_pop_minus_emigration_with_transitions <-
       prev_pop_minus_emigration_with_transitions |>
       filter(state_name != "Died")
 
-    print(paste0("Deaths: ",died |> summarise(a=sum(population)) |> pull(a) |>round()))
+    # log list
+    log_list <- log_and_print_intermediate_results(
+      log_list,
+      paste0("Deaths: ",
+             died |> summarise(a=sum(population)) |> pull(a) |>round()),
+      print_intermediate_results)
+
 
     #####
     # here come some births
@@ -174,12 +218,20 @@ run_dpm_age_based <- function(folder){
       summarise(a=sum(value)) |> pull(a)
     pop_prev_m_e_w_t <- prev_pop_minus_emigration_with_transitions |>
       summarise(a=sum(population)) |> pull(a)
-    pop_prev_m_e_w_t_a_b <- prev_pop_minus_emigration_with_transitions_and_births |>
+    pop_prev_m_e_w_t_a_b <-
+      prev_pop_minus_emigration_with_transitions_and_births |>
       summarise(a=sum(population)) |> pull(a)
     births_actual <- pop_prev_m_e_w_t_a_b - pop_prev_m_e_w_t
 
-    print(paste0(round(births_expected)," births expected"))
-    print(paste0(round(births_actual)," births actual"))
+
+    # log list
+    log_list <- log_and_print_intermediate_results(
+      log_list,
+      paste0("Births: ",
+             round(births_expected)," expected, ",
+      round(births_actual)," actual"),
+      print_intermediate_results)
+
 
 
     #####
@@ -202,14 +254,22 @@ run_dpm_age_based <- function(folder){
       births_net_migration_deaths_by_CS |>
       filter(event%in%c("immigrations"),year==i, value!=0) |>
       summarise(a=sum(value)) |> pull(a)
-    pop_prev_m_e_w_t_a_b_a_e <- prev_pop_minus_emigration_with_transitions_and_births_and_immigration |>
+    pop_prev_m_e_w_t_a_b_a_e <-
+      prev_pop_minus_emigration_with_transitions_and_births_and_immigration |>
       summarise(a=sum(population)) |> pull(a)
     immigrations_actual <- pop_prev_m_e_w_t_a_b_a_e - pop_prev_m_e_w_t_a_b
 
-    print(paste0(round(immigrations_expected)," immigrations expected"))
-    print(paste0(round(immigrations_actual)," immigrations actual"))
+    # log list
+    log_list <- log_and_print_intermediate_results(
+      log_list,
+      paste0("Immigrations: ",
+             round(immigrations_expected)," expected, ",
+             round(immigrations_actual)," actual"),
+      print_intermediate_results)
 
-    if(min(prev_pop_minus_emigration_with_transitions_and_births_and_immigration$population) < 0){
+    if(prev_pop_minus_emigration_with_transitions_and_births_and_immigration |>
+       pull(population) |>
+       min() < 0){
       zero_year_check <- zero_year_check + 1
       # tare it to be 0 as that's the min
       prev_pop_minus_emigration_with_transitions_and_births_and_immigration <-
@@ -217,8 +277,10 @@ run_dpm_age_based <- function(folder){
         mutate(population = ifelse(population<0,0,population))
     }
     population_at_each_year <-
-      bind_rows(population_at_each_year,
-                prev_pop_minus_emigration_with_transitions_and_births_and_immigration |>  mutate(year=i)) |>
+      bind_rows(
+        population_at_each_year,
+        prev_pop_minus_emigration_with_transitions_and_births_and_immigration |>
+          mutate(year=i)) |>
       select(year, age, age_group, state_name, population) |>
       arrange(year, age, age_group, state_name)
   }
